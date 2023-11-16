aa24d5406faa287570ec23d17f68c437a70c41ac
jmathison21 <jennamath21@gmail.com>
2023-11-15 19:19:11 -0500

    tabcontent

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -3,19 +3,44 @@ import { useEffect, useState } from "react";
 import db from "src/jennaDB.js"
 
 
+//Tab Componenent Functions
+const tabComponents = {
+  Social: <Social />,
+  Bio: <Bio />,
+  Projects: <Projects />,
+  Resume: <Resume/>,
+}
+
+function Social() {
+  return(<p>Empty Social</p>)
+}
+
+function Bio() {
+  return(<p>Empty Bio</p>)
+}
+
+function Projects() {
+  return(<p>Empty Projects</p>)
+}
+
+function Resume () {
+  return(<p>Empty Resume</p>)
+}
+
+
+//Page Components
 function Header() {
   return (
     <div>
       <h1>Jenna Mathison</h1>
     </div>
   );
-  }
+}
 
 function View({tab}) {
-  const tabContent = db.getTabContent(tab)
-  console.log(tab,tabContent)
+  const tabComponent = tabComponents[tab]
 
-  return(<p>{tabContent}</p>)
+  return(tabComponent)
 }
 
 function Content() {
@@ -24,12 +49,10 @@ function Content() {
   const tabs = tabsList.map(name => <button key={name} onClick={() => {setTab(name)}}>{name}</button>)
 
   return (
-    <>
     <div>
       <div>{tabs}</div>
-      <div id="content"><View tab={tab} /></div>
+      <div><View tab={tab} /></div>
     </div>
-    </>
   );
 }
 
